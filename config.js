3908eed0c0e28ecddc698b1884cd9e58d362b95a
ReZo14 <53716874+ReZo14@users.noreply.github.com>
2020-02-11 07:21:05 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: "Njc1MDM2NTYwNzgxMDgyNjcz.XkAEwA.7VyVWNsaLzWxE1PCpULpg8MyfmE",    
+    YT_API_KEY: 'api ', 
+    prefix: 'v!',
+    devs: ['535750351748595739','619902756161060904']
+}
